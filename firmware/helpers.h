30e26b5d39dda41050b4ba72b39602a2b5bdbc9f
Tomasz Wasilczyk <tomasz@wasilczyk.pl>
2026-01-07 17:53:19 -0800

    Found TDS F-codes and buffer overrun

--- firmware/helpers.h
+++ firmware/helpers.h
@@ -21,3 +21,18 @@ uint8_t bflip(uint8_t byte, uint8_t byte_no, uint8_t flip_idx) {
 
     return byte ^ (1 << (flip_idx % 8));
 }
+
+// idx = 0..6
+uint16_t tdsExploit(uint8_t idx) {
+    uint16_t tds = 20 + idx * 4;
+    tds *= 1000;
+    return tds;
+}
+
+uint8_t tdsExploitHi(uint8_t code) {
+    return tdsExploit(code) >> 8;
+}
+
+uint8_t tdsExploitLo(uint8_t code) {
+    return tdsExploit(code) & 0xFF;
+}
