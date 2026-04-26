6ac724933b37527b1880597bd91bea9430fb61f1
Tomasz Wasilczyk <tomasz@wasilczyk.pl>
2026-04-25 22:12:55 -0700

    Serial protocol: RO communication loop

--- firmware/components/waterdrop_serial_faucet/waterdrop_serial_faucet.cpp
+++ firmware/components/waterdrop_serial_faucet/waterdrop_serial_faucet.cpp
@@ -5,9 +5,60 @@
 
 namespace esphome::waterdrop_serial::faucet {
 
+using namespace std::chrono_literals;
+
 static const char *const TAG = "waterdrop_serial_faucet";
 
 WaterdropSerialFaucet::WaterdropSerialFaucet() : WaterdropSerial(frame::Source::FAUCET) {}
+
+void WaterdropSerialFaucet::loop() {
+  WaterdropSerial::loop();
+
+  const auto now = std::chrono::steady_clock::now();
+  if (now >= next_command_at_) {
+    switch (next_command_) {
+      case frame::Command::COMMAND_C2:
+        next_command_at_ = now + 80ms;
+        next_command_ = frame::Command::COMMAND_C5;
+        send_c2_();
+        break;
+      case frame::Command::COMMAND_C5:
+        next_command_at_ = now + 90ms;
+        next_command_ = frame::Command::COMMAND_C2;
+        send_c5_();
+        break;
+      default:
+        assert(false);
+    }
+  }
+}
+
+void WaterdropSerialFaucet::send_c2_() {
+  // STATE:
+  //  Boot: FF
+  //  Screen on: F1, F7
+  //  Flush: F3, F5, F6
+  //  Screen off: all others
+
+  // FLG2: no effect
+  //  0x03 - regular
+  //  0x01 - present during flush and boot
+
+  // ERR:
+  //  Display priority: E02 > E03 > E04 > E01
+  //  0b00000100: E02
+  //  0b00000001: E03
+  //  0b00000010: E04
+  //  0b10000000: E01
+
+  //                                                         STATE   FLG   ERR
+  send_frame(frame::Command::COMMAND_C2, std::vector<uint8_t>{0xF1, 0x03, 0x00});
+}
+
+void WaterdropSerialFaucet::send_c5_() {
+  send_frame(frame::Command::COMMAND_C5, cc5_data_.at(cc5_counter_++));
+  cc5_counter_ %= cc5_data_.size();
+}
 
 void WaterdropSerialFaucet::dump_config() {
   ESP_LOGCONFIG(TAG, "Waterdrop Serial Faucet:");
@@ -16,13 +67,32 @@ void WaterdropSerialFaucet::dump_config() {
 
 void WaterdropSerialFaucet::handle_frame_(const frame::Frame &frame) {
   ESP_LOGD(TAG, "RX %s", frame.to_string().c_str());
+  if (frame.command != frame::Command::COMMAND_22) {
+    ESP_LOGW(TAG, "Unexpected request: %02X", static_cast<uint8_t>(frame.command));
+    return;
+  }
+  if (frame.payload_length != 8) {
+    ESP_LOGW(TAG, "Invalid request length: %d", frame.payload_length);
+    return;
+  }
+
+  uint8_t page = frame.payload[0];
+  if (c22_data_.count(page) == 0) {
+    ESP_LOGW(TAG, "Unknown page requested: %02X", page);
+    return;
+  }
+  send_frame(frame::Command::COMMAND_22, c22_data_.at(page));
+  if (page == 3) {
+    if (c22_data_[page][6] == 255) c22_data_[page][5]++;
+    c22_data_[page][6]++;
+  }
 }
 
-void WaterdropSerialFaucet::send_frame(uint8_t command, const std::vector<uint8_t> &payload) {
+void WaterdropSerialFaucet::send_frame(frame::Command command, const std::vector<uint8_t> &payload) {
   frame::Frame frame;
   assert(payload.size() <= frame.payload.size());
   frame.source = frame::Source::RO;
-  frame.command = static_cast<frame::Command>(command);
+  frame.command = command;
   frame.payload_length = static_cast<uint8_t>(payload.size());
   std::copy(payload.begin(), payload.end(), frame.payload.begin());
 
--- firmware/components/waterdrop_serial_faucet/waterdrop_serial_faucet.h
+++ firmware/components/waterdrop_serial_faucet/waterdrop_serial_faucet.h
@@ -3,7 +3,9 @@
 #include "esphome/components/waterdrop_serial/frame.h"
 #include "esphome/components/waterdrop_serial/waterdrop_serial.h"
 
+#include <chrono>
 #include <cstdint>
+#include <map>
 #include <vector>
 
 namespace esphome::waterdrop_serial::faucet {
@@ -12,10 +14,66 @@ class WaterdropSerialFaucet : public WaterdropSerial {
  public:
   WaterdropSerialFaucet();
   void dump_config() override;
-  void send_frame(uint8_t command, const std::vector<uint8_t> &payload);
+  void loop() override;
 
  private:
   void handle_frame_(const frame::Frame &frame) override;
+
+  // TODO: remove
+  void send_frame(frame::Command command, const std::vector<uint8_t> &payload);
+
+  void send_c2_();
+  void send_c5_();
+
+  frame::Command next_command_ = frame::Command::COMMAND_C2;
+  std::chrono::steady_clock::time_point next_command_at_ = {};
+
+  uint8_t cc5_counter_ = 0;
+  std::vector<std::vector<uint8_t>> cc5_data_{
+    // A: 00 (rare) or 0A
+    //                                            A
+    {0x01, 0x00, 0x40, 0x22, 0x00, 0x00, 0x00, 0x0A},
+
+    // A: 00, 34, 35
+    //                         A
+    {0x02, 0x00, 0x00, 0x00, 0x35, 0x08, 0x34, 0x00},
+
+    // A: 0C, 0E, 0D (rare), 19 (rare)
+    // B: 01, 03
+    // C: 90, B0
+    //             A     B     C
+    {0x03, 0x00, 0x0E, 0x01, 0xB0, 0x00, 0x0A, 0x00},
+
+    // A: F1, F3, FF
+    // B: F1, F3
+    //       A     B
+    {0x04, 0xF1, 0xF1, 0x00, 0x00, 0x00, 0x0E, 0xFF},
+    {0x05, 0x00, 0x00, 0x0E, 0x0E, 0x00, 0x00, 0x00},
+  };
+
+  std::map<uint8_t, std::vector<uint8_t>> c22_data_{
+    {0x0E, {0x0E, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x11}},
+
+    // CF, RO, CB (H, L): filter total life (when it turns red).
+    //            >- CF   -<  >-  RO  -<  >-  CB  <-
+    {0x0F, {0x0F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFe, 0x99, 0xFF, 0x33}},
+
+    // TDS: 1-999 value with 1000-9999 displaying as F-codes.
+    // A: 0C or 0E
+    //                                   |  H  TDS L|         A
+    {0x03, {0x03, 0x00, 0x00, 0x00, 0x00,    0,    1, 0x00, 0x0C}},
+
+    // CF, RO, CB (H, L): filter used life. Turns orange at 360 units before red
+    //            >- CF   -<  >-  RO  -<  >-  CB  <-
+    {0x02, {0x02, 0xFE, 0x96, 0xFE, 0x97, 0xFE, 0x98, 0x00, 0x00}},
+
+    // A: 0A or 00 (rare) - A always the same and same in 01 and 0D slots
+    //                    A           A
+    {0x0D, {0x0D, 0x00, 0x0A, 0x00, 0x0A, 0x00, 0x0A, 0x00, 0x00}},
+
+    // Seems to be the same as 0D slot
+    {0x01, {0x01, 0x00, 0x0A, 0x00, 0x0A, 0x00, 0x0A, 0x00, 0x00}},
+  };
 };
 
 }  // namespace esphome::waterdrop_serial::faucet
